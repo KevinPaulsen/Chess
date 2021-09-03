752cf9b844ba1ec5dcd9a980371f2fb9184c7b7a
Kevin <kevindpaulsen@gmail.com>
2021-09-02 23:28:29 -0700

    Added documentation to Evaluator

--- app/src/main/java/chess/model/chessai/Evaluator.java
+++ app/src/main/java/chess/model/chessai/Evaluator.java
@@ -7,7 +7,22 @@ import java.util.List;
 
 public interface Evaluator {
 
+    /**
+     * Evaluates the given game and returns an evaluation of this position. The
+     * Evaluation will have a depth of 0, and the move will be null.
+     *
+     * @param game the game to evaluate.
+     * @return the evaluation of this game.
+     */
     Evaluation evaluate(GameModel game);
 
+    /**
+     * Returns a list of all the legal moves in this position, and they are sorted
+     * into this evaluators best guess from most-likely to be the best move, to least
+     * likely.
+     *
+     * @param game The game to get moves from.
+     * @return the list of sorted legal moves.
+     */
     List<Move> getSortedMoves(GameModel game);
 }
